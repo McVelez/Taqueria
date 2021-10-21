1f477a0f3ae228103d6280c251613faa7208f32a
Marco Velez <61840865+McVelez@users.noreply.github.com>
2021-10-21 12:26:46 -0700

    Update Taqueria_Main.py

--- Taqueria_Main.py
+++ Taqueria_Main.py
@@ -195,7 +195,7 @@ def individualTaqueroMethod(taquero):
 def cookFood(taquero, ingredients):
     for ing in ingredients:
         sleep(TAQUERO_WAITING_TIME[ing]) # Nota: Marco no cree que esto funcione
-    
+    taquero.tacoCounter += 1
     if taquero.tacoCounter == taquero.tacosNeededForRest:
         return True
     return False
